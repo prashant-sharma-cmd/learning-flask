8bebf916cbde956009c155b9c8aba834364deb80
Admin <prashant.shar51@gmail.com>
2026-02-03 18:24:13 +0800

    User Authenticatio

--- fifthapp/app.py
+++ fifthapp/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 db = SQLAlchemy()
 
@@ -9,12 +12,27 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres
     # @localhost:5432/flasktutorialdb '
-
+    app.secret_key = "SOME RANDOM KEY"
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    from models import User
+
+    @login_manager.user_loader
+    def load_user(uid):
+        return User.query.get(uid)
+
+    @login_manager.unauthorized_handler
+    def unauthorized_callback():
+        return redirect(url_for('home'))
+
+    bcrypt = Bcrypt(app)
+
     from routes import register_routes
-    register_routes(app, db)
+    register_routes(app, db, bcrypt)
 
     migrate = Migrate(app, db)
 
--- fifthapp/routes.py
+++ fifthapp/routes.py
@@ -1,9 +1,57 @@
-from flask import render_template, request
-from models import Person
+from flask import render_template, request, redirect, url_for
+from models import Person, User
+from flask_login import login_user, logout_user, current_user, login_required
 
-def register_routes(app, db):
+def register_routes(app, db, bcrypt):
 
     @app.route('/', methods=['GET', 'POST'])
+    def home():
+        return render_template('home.html')
+
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'GET':
+            return render_template('signup.html')
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            hashed_password = bcrypt.generate_password_hash(password)
+
+            user = User(username=username, password=hashed_password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('home'))
+        return ""
+
+    @app.route('/login/', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            user = User.query.filter_by(username=username).first()
+
+            if bcrypt.check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('home'))
+            else:
+                return render_template('login.html')
+
+        return ""
+
+
+    @app.route('/logout/', methods=['GET', 'POST'])
+    def logout():
+        logout_user()
+        return redirect(url_for('home'))
+
+    @app.route('/index', methods=['GET', 'POST'])
+    @login_required
     def index():
         if request.method == 'GET':
             people = Person.query.all()
