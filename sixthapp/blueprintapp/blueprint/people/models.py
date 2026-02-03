80390b49ad9fa1cf81084bcf4e66af4871d528ec
Admin <prashant.shar51@gmail.com>
2026-02-03 20:23:25 +0800

    Blueprints

---
+++ sixthapp/blueprintapp/blueprint/people/models.py
@@ -0,0 +1,15 @@
+from blueprintapp.app import db
+
+class Person(db.Model):
+    __tablename__ = 'people'
+
+    pid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    job = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<PERSON {self.name}, AGE: <{self.age}>"
+
+    def get_id(self):
+        return self.pid
